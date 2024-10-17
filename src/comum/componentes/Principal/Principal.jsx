3523ca38ca36a80cf035cc3a83901a83100ac46a
LeandrojLemes <leandroolemes22@gmail.com>
2024-10-16 21:07:19 -0300

    ListaDeExercicios

--- src/comum/componentes/Principal/Principal.jsx
+++ src/comum/componentes/Principal/Principal.jsx
@@ -1,22 +1,24 @@
-import { Link } from 'react-router-dom';
-import BotaoCustomizado from '../BotaoCustomizado/BotaoCustomizado';
-import './Principal.css'
+import { Link } from "react-router-dom";
+import BotaoCustomizado from "../BotaoCustomizado/BotaoCustomizado";
+import "./Principal.css";
+import { TiArrowBack } from "react-icons/ti";
 
 function Principal({ voltarPara, titulo, children }) {
+  return (
+    <main className="principal_root">
+      <div className="principal_titulo">
+        {voltarPara && (
+          <Link to={voltarPara}>
+            <TiArrowBack size={20} />
+          </Link>
+        )}
 
-    return (
-        <main className='principal_root'>
-            <div className='principal_titulo'>
-                {voltarPara && <Link to={voltarPara}>Voltar</Link>}
+        <h1>{titulo}</h1>
+      </div>
 
-                <h1>{titulo}</h1>
-
-            </div>
-
-            {children}
-
-        </main>
-    );
+      {children}
+    </main>
+  );
 }
 
 export default Principal;
--- src/paginas/PaginaInicial/PaginaListaTarefas/PaginaListaTarefas.jsx
+++ src/paginas/PaginaInicial/PaginaListaTarefas/PaginaListaTarefas.jsx
@@ -2,35 +2,62 @@ import { useState } from "react";
 import BotaoCustomizado from "../../../comum/componentes/BotaoCustomizado/BotaoCustomizado";
 import Principal from "../../../comum/componentes/Principal/Principal";
 
+import { AiFillRest } from "react-icons/ai";
+
 const PaginaListaTarefas = () => {
-    const [descricao, setDescricao] = useState('');
-    const [tarefas, setTarefas] = useState([]);
-    const adicionarNaLista = () => {
-        setTarefas([...tarefas, descricao])
-        setDescricao('')
+  const [descricao, setDescricao] = useState("");
+  const [tarefas, setTarefas] = useState([]);
 
-    };
+  const adicionarNaLista = () => {
+    if (descricao && descricao.trim()) {
+      tarefas.push(descricao);
+      setTarefas([...tarefas]);
+      setDescricao("");
+      document.getElementById("campoDescricao").focus();
+    } else {
+      alert("Preencha o campo descricao");
+    }
+  };
+  const removerDaLista = (index) => {
+    tarefas.splice(index, 1);
+    setTarefas([...tarefas]);
+  };
 
-    return <Principal titulo="Lista de Tarefas" voltarPara="/">
-        <div>
-            <input
-                type="text"
-                value={descricao}
-                onChange={(e) => setDescricao(e.target.value)}
-            />
+  return (
+    <Principal titulo="Lista de Tarefas" voltarPara="/">
+      <div>
+        <input
+          id="campoDescricao"
+          type="text"
+          placeholder="Descrição da Tarefa"
+          value={descricao}
+          onChange={(e) => setDescricao(e.target.value)}
+          onKeyUp={(e) => {
+            if (e.key === "Enter") {
+              adicionarNaLista();
+            }
+          }}
+        />
 
-            <BotaoCustomizado aoClicar={adicionarNaLista}>
-                +
-            </BotaoCustomizado>
-        </div>
+        <BotaoCustomizado aoClicar={adicionarNaLista}>+</BotaoCustomizado>
+      </div>
 
-        <ul>
-            {tarefas.map((item, index) => {
-                return <li key={index}>{item}</li>;
-            })}
-        </ul>
-
-    </Principal>;
+      <ul>
+        {tarefas.map((item, index) => {
+          return (
+            <li key={index}>
+              {item}
+              <AiFillRest
+                color="gray"
+                size={20}
+                onClick={() => removerDaLista(index)}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </Principal>
+  );
 };
 
 export default PaginaListaTarefas;
